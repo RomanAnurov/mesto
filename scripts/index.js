464253553a271666824390a23906eaf04bdfc0c0
Roman Anurov <anurovroman@yandex.ru>
2022-05-27 14:01:05 +0300

    Кнопка лайка и добавление карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,25 @@
-const openPopupButton = document.querySelector('.profile__edit-button');
-const popupEdit = document.querySelector('.popup_type_edit-profile');
-const closePopupButton = popupEdit.querySelector('.popup__close');
+const openPopupButton = document.querySelector(".profile__edit-button");
+const popupEditButton = document.querySelector(".popup_type_edit-profile");
+const closePopupButton = popupEditButton.querySelector(".popup__close");
 
 function popupOpenToggle() {
-  popupEdit.classList.toggle('popup_type_opened');
-  
+  popupEditButton.classList.toggle("popup_type_opened");
 }
 
+openPopupButton.addEventListener("click", popupOpenToggle);
 
-openPopupButton.addEventListener('click', popupOpenToggle);
-
-closePopupButton.addEventListener('click', popupOpenToggle);
-
+closePopupButton.addEventListener("click", popupOpenToggle);
 
 //редактирование формы//
 
 // Находим форму в DOM
-const formElement = popupEdit.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+const formElement = popupEditButton.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-const nameInput = formElement.querySelector('.popup__input_type_user-name'); // Воспользуйтесь инструментом .querySelector()
-const jobInput = formElement.querySelector('.popup__input_type_user-about'); // Воспользуйтесь инструментом .querySelector()
+const nameInput = formElement.querySelector(".popup__input_type_user-name"); // Воспользуйтесь инструментом .querySelector()
+const jobInput = formElement.querySelector(".popup__input_type_user-about"); // Воспользуйтесь инструментом .querySelector()
 
-const profileTitle = document.querySelector('.profile__title');
-const profileSubtitle = document.querySelector('.profile__sub-title');
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__sub-title");
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -32,7 +29,6 @@ function formSubmitHandler(evt) {
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
 
-
   // Получите значение полей jobInput и nameInput из свойства value
 
   // Выберите элементы, куда должны быть вставлены значения полей
@@ -41,93 +37,119 @@ function formSubmitHandler(evt) {
   profileTitle.textContent = `${nameInput.value}`;
   profileSubtitle.textContent = `${jobInput.value}`;
 
-  return popupOpenToggle()
+  return popupOpenToggle();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
 
 // второй ПопАп //
 
-const openPopupAddButton = document.querySelector('.profile__add-button');
-const popupAdd = document.querySelector('.popup_type_add-card');
-const closePopupAddButton = popupAdd.querySelector('.popup__close');
+const openPopupAddButton = document.querySelector(".profile__add-button");
+const popupAdd = document.querySelector(".popup_type_add-card");
+const closePopupAddButton = popupAdd.querySelector(".popup__close");
 
 function popupAddOpenToggle() {
-  popupAdd.classList.toggle('popup_type_opened');
+  popupAdd.classList.toggle("popup_type_opened");
 }
 
-openPopupAddButton.addEventListener('click', popupAddOpenToggle);
-closePopupAddButton.addEventListener('click', popupAddOpenToggle);
+openPopupAddButton.addEventListener("click", popupAddOpenToggle);
+closePopupAddButton.addEventListener("click", popupAddOpenToggle);
 
-const initialCards = [{
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
-//Дом элементы//
+// Шаблоны
+const elementsCardTemplate = document
+  .querySelector("#elements-template")
+  .content.querySelector(".elements__item");
 
-const elementsContainer = document.querySelector('.elements');
-const formAddCard = popupAdd.querySelector('.popup__form');
-const nameCardInput = formAddCard.querySelector('.popup__input_type_card-name');
-const urlCardInput = formAddCard.querySelector('.popup__input_type_card-url');
+//Дом элементы
 
-// Обработчики событий//
+const elementsContainer = document.querySelector(".elements");
+const formAddCard = popupAdd.querySelector(".popup__form");
+const nameCardInput = formAddCard.querySelector(".popup__input_type_card-name");
+const urlCardInput = formAddCard.querySelector(".popup__input_type_card-url");
+
+
+// Обработчики событий
 
 const handleSubmitAddElementsForm = (evt) => {
   evt.preventDefault();
-  renderElementsList ({name: nameCardInput.value, link: urlCardInput.value});
-  nameCardInput.value = 'Название';
-  urlCardInput.value = 'Ссылка на картинку';
-   return popupAddOpenToggle();
+  renderElementsList({
+    name: nameCardInput.value,
+    link: urlCardInput.value,
+  });
+  nameCardInput.value = "Название";
+  urlCardInput.value = "Ссылка на картинку";
+  return popupAddOpenToggle();
+};
+
+const handleDeleteElementsCard = (evt) => {
+  evt.target.closest(".elements__item").remove();
+};
+
+const handleLikeElementsCard = (evt) => {
+  evt.target.classList.toggle("elements__icon_active");
+};
 
 
+// генерация карточки
+
+function generateElementsCard(elementsData) {
+  const newElementsCard = elementsCardTemplate.cloneNode(true);
+
+  const nameElementsCard = newElementsCard.querySelector(".elements__caption");
+  const linkElementsCard = newElementsCard.querySelector(".elements__foto");
+
+  nameElementsCard.textContent = elementsData.name;
+  linkElementsCard.src = elementsData.link;
+
+  const deleteButton = newElementsCard.querySelector(".elements__icon-basket");
+  deleteButton.addEventListener("click", handleDeleteElementsCard);
+
+
+  const likeButton = newElementsCard.querySelector(".elements__icon");
+  likeButton.addEventListener("click", handleLikeElementsCard);
+
+
+
+  return newElementsCard;
 }
 
-formAddCard.addEventListener('submit', handleSubmitAddElementsForm);
-
-//рендер карточки//
+//рендер карточки
 
 const renderElementsList = (elementsData) => {
-  elementsContainer.insertAdjacentHTML('afterbegin',
-    `<div class="elements__item">
-  <img class="elements__foto" src=${elementsData.link} alt=${elementsData.name}>
-  <div class="elements__info">
-    <h2 class="elements__caption">${elementsData.name}</h2>
-    <button class="elements__icon" type="button"></button>
-  </div>
-</div>`
-  );
-}
+  elementsContainer.prepend(generateElementsCard(elementsData));
+};
 
 initialCards.forEach((elementsData) => {
   renderElementsList(elementsData);
-})
+});
 
-
-
-
-
-
+formAddCard.addEventListener("submit", handleSubmitAddElementsForm);
